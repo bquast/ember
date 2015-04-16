26db4d04a33188b8a9a8e54f4a1fbdae047b39dd
Bastiaan Quast <bquast@gmail.com>
2015-04-16 18:48:06 +0200

    finish htmlwidget, add code for renderValue t.b. changed to ember

--- inst/htmlwidgets/ember.js
+++ inst/htmlwidgets/ember.js
@@ -6,20 +6,42 @@ HTMLWidgets.widget({
 
   initialize: function(el, width, height) {
 
+    // create our ember object and bind it to the element
+    var emb = new ember(el.id);
+
+    // TODO: add instance fields as required
     return {
-      // TODO: add instance fields as required
+      emb: emb
     }
 
   },
 
   renderValue: function(el, x, instance) {
 
-    el.innerText = x.message;
+    // parse gexf data
+    var parser = new DOMParser();
+    var data = parser.parseFromString(x.data, "application/xml");
 
+    // apply settings
+    for (var name in x.settings)
+      instance.sig.settings(name, x.settings[name]);
+
+    // update the sigma instance
+    sigma.parsers.gexf(
+      data,          // parsed gexf data
+      instance.sig,  // sigma instance we created in initialize
+      function() {
+        // need to call refresh to reflect new settings and data
+        instance.sig.refresh();
+      }
+    );
   },
 
   resize: function(el, width, height, instance) {
 
+    // forward resize on to sigma renderers
+    for (var name in instance.sig.renderers)
+      instance.sig.renderers[name].resize(width, height);
   }
 
 });
